aec022414471011b4a47b0a8d71fce9a5a8f1a33
Fabian Parzefall <fabian.parzefall@mailbox.org>
2018-01-20 11:54:18 +0100

    Add version 1.2.0

---
+++ FPar.GEXF/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GEXF")]
+[assembly: AssemblyDescription("GEXF C# XML serialization classes.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyProduct("Gephi")]
+[assembly: AssemblyCompany("Fabian Parzefall")]
+[assembly: AssemblyCopyright("Copyright © Fabian Parzefall 2018")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.2.0")]
---
+++ README.md
@@ -0,0 +1,21 @@
+GEXF C# XML serialization classes
+=================================
+
+This package contains XML serialization classes for the GEXF graph storage
+format.
+
+Usage
+-----
+
+This package can be installed with the NuGet package manager:
+
+```
+PM> Install-Package FPar.GEXF
+```
+
+Related links
+-------------
+
+- NuGet package: https://www.nuget.org/packages/FPar.GEXF/
+- GEXF documentation: https://gephi.org/gexf/format/
+- GEXF specifications repository: https://github.com/gephi/gexf/
